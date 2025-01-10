dde305a520225edb5a8692a215bd7190623ad336
Muhammad Dicky Andeyan Naratama <dickyandeyan@gmail.com>
2025-01-10 22:24:29 +0700

    [init]: Add Tailwind CSS integration and remove unused components

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+export default function Index() {
+  return (
+    <View className="flex items-center h-full justify-center">
+      <Text className="text-4xl">⚛️</Text>
+      <Text className="text-4xl text-center leading-snug font-extrabold">
+        Welcome to React Native Starter
+      </Text>
+    </View>
+  )
+}
